a32d11406cf61b7149f1a2d6aa8e1fae57ed1d64
Torin Nelson <nelsontorin1@gmail.com>
2024-05-16 02:58:52 -0700

    clustering coefficient

--- graph_algorithms.py
+++ graph_algorithms.py
@@ -18,7 +18,12 @@ def get_diameter(graph: Graph) -> int:
 
 
 def get_clustering_coefficient(graph: Graph) -> float:
-	raise NotImplementedError
+    triangles = num_triangles(graph)
+    two_edges = num_two_edge(graph)
+    if two_edges == 0:
+        return 0.0
+    return 3 * triangles / two_edges
+
 
 
 def get_degree_distribution(graph: Graph) -> dict[int, int]:
@@ -50,3 +55,61 @@ def bfs(graph, r):
 					max_dist = dist[i]
 
 	return s, max_dist
+
+def num_two_edge(graph):
+    num_two_edge = 0
+    for node in graph.nodes:
+        degree = len(graph.get_neighbors(node))
+        num_two_edge += degree * (degree - 1) // 2
+    return num_two_edge
+
+def num_triangles(graph):
+	L, degeneracy, N = degree_degeneracy(graph)
+	triangle_count = 0
+	for v in L:
+		for u in N[v]:
+			for w in N[v]:
+				if (u,w) in graph.edges:
+					triangle_count += 1
+	return triangle_count 
+
+def compute_degrees(graph):
+	degree_dict = {}
+	for node in graph.nodes:
+		degree_dict[node] = len(graph.get_neighbors(node))
+	return degree_dict
+
+def degree_degeneracy(graph):
+    L = []
+    degree = compute_degrees(graph)
+    D = [[] for _ in range(len(graph.nodes) + 1)]
+    H = set()
+    N = {v: [] for v in graph.nodes}
+    for v, d in degree.items():
+        D[d].append(v)
+
+    k = 0
+
+    for _ in range(len(graph.nodes)):
+        i = 0
+        while not D[i]:
+            i += 1
+        k = max(k, i)
+        v = D[i].pop()
+        L.insert(0, v)
+        H.add(v)
+        for w in graph.get_neighbors(v):
+            if w not in H:
+                old_degree = degree[w]
+                new_degree = old_degree - 1
+                degree[w] = new_degree
+                D[old_degree].remove(w)
+                D[new_degree].append(w)
+                N[v].append(w)
+
+    return L, k, N
+
+
+
+
+
